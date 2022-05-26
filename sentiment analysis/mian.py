3e637dd0c2318554fc6eb15c027f82bbad1572fd
giovanniminchio98 <gminchiogm@gmail.com>
2022-05-26 23:14:12 +0200

    sentiment analysis first test and starting point

---
+++ sentiment analysis/mian.py
@@ -0,0 +1,136 @@
+'imports'
+from http.client import responses
+import os
+import pandas as pd
+import numpy as np
+import re
+import flair
+
+# to interact with twitter api
+import requests
+
+BEARER_TOKEN='ghesboro accettatemi'
+
+#functions
+def get_data(tweet):
+    data = {
+        'id': tweet['id_str'],
+        'created_at': tweet['created_at'],
+        'text': tweet['full_text']
+    }
+    return data
+
+if __name__=='_-main__':
+
+    # example of request for twitter tweet info request
+    params = {'q': 'tesla',
+          'tweet_mode': 'extended'}
+    requests.get(
+        'https://api.twitter.com/1.1/search/tweets.json',
+        params=params,
+        headers={'authorization': 'Bearer '+BEARER_TOKEN}
+    )
+
+    # better params with more info
+    # example get only english tweet
+    params = {
+        'q': 'tesla',
+        'tweet_mode': 'extended',
+        'lang': 'en',
+        'count': '100' # minimum number of tweet to return
+    }
+
+    # Building Our Dataset
+    # Once we have our API request setup, we can begin 
+    # running it to populate our dataset.
+    df = pd.DataFrame()
+    for tweet in responses.json()['statuses']:
+        row = get_data(tweet)
+        df = df.append(row, ignore_index=True)
+
+    print(df.heaf())
+
+    # Sentiment Analysis
+    # We will be using a pre-trained sentiment analysis 
+    # model from the flair library. As far as 
+    # pre-trained models go, this is one of the most powerful.
+
+    # This model splits the text into character-level tokens
+    #  and uses the DistilBERT model to make predictions.
+
+    # The advantage of working at the character-level 
+    # (as opposed to word-level) is that words that the 
+    # network has never seen before can still be assigned 
+    # a sentiment.
+
+    # Flair
+    # To use the flair model, we first need to 
+    # import the library with pip install flair. 
+    # Once installed, we import and initialize the model like so:
+
+    import flair
+    sentiment_model = flair.models.TextClassifier.load('en-sentiment')
+
+    # All we need to do now is tokenize our text by passing 
+    # it through flair.data.Sentence(<TEXT HERE>) and calling the .
+    # predict method on our model. Putting those together, we get:
+
+    TEXT = 'prova prova'
+
+    sentence = flair.data.Sentence(TEXT)
+    sentiment_model.predict(sentence)
+
+    # Analyzing Tesla Tweets
+    # Most of our tweets are very messy. 
+    # Cleaning text data is fundamental, although we will just 
+    # do the bare minimum in this example.
+
+    whitespace = re.compile(r"\s+")
+    web_address = re.compile(r"(?i)http(s):\/\/[a-z0-9.~_\-\/]+")
+    tesla = re.compile(r"(?i)@Tesla(?=\b)")
+    user = re.compile(r"(?i)@[a-z0-9_]+")
+
+    # we then use the sub method to replace anything matching
+    tweet = whitespace.sub(' ', tweet)
+    tweet = web_address.sub('', tweet)
+    tweet = tesla.sub('Tesla', tweet)
+    tweet = user.sub('', tweet)
+
+    # Now we have our clean(ish) tweet — we can tokenize it by 
+    # converting it into a sentence object, and then predict the sentiment:
+
+    sentence = flair.data.Sentence(tweet)
+    sentiment_model.predict(sentence)
+
+    # Finally, we extract our predictions and add them to our tweets dataframe.
+    #  We can access the label object (the prediction) by typing sentence.labels[0]. 
+    # With this, we call score to get our confidence/probability score, 
+    # and value for the POSITIVE/NEGATIVE prediction:
+
+    probability = sentence.labels[0].score  # numerical value 0-1
+    sentiment = sentence.labels[0].value  # 'POSITIVE' or 'NEGATIVE'
+
+    # We can append the probability and sentiment to lists
+    #  which we then merge with our tweets dataframe. 
+    # Putting all of these parts together will give us:
+
+    # we will append probability and sentiment preds later
+    probs = []
+    sentiments = []
+
+    # use regex expressions (in clean function) to clean tweets
+    tweets['text'] = tweets['text'].apply(clean)
+
+    for tweet in tweets['text'].to_list():
+        # make prediction
+        sentence = flair.data.Sentence(tweet)
+        sentiment_model.predict(sentence)
+        # extract sentiment prediction
+        probs.append(sentence.labels[0].score)  # numerical score 0-1
+        sentiments.append(sentence.labels[0].value)  # 'POSITIVE' or 'NEGATIVE'
+
+    # add probability and sentiment predictions to tweets dataframe
+    tweets['probability'] = probs
+    tweets['sentiment'] = sentiments
+
+    print('madre')
---
+++ sentiment analysis/test.py
@@ -0,0 +1,52 @@
+import os
+import flair
+import numpy as np
+
+
+if __name__ == '__main__':
+    print('inizio test...')
+
+    sentiment_model = flair.models.TextClassifier.load('en-sentiment')
+    a=1
+
+    sentence = flair.data.Sentence('im happy')
+    sentiment_model.predict(sentence)
+    probability = sentence.labels[0].score  # numerical value 0-1
+    sentiment = sentence.labels[0].value  # 'POSITIVE' or 'NEGATIVE'
+    print(probability)
+    print(sentiment)
+
+
+
+    'test google translator api '
+    from googletrans import Translator
+
+    translator = Translator()
+
+    frasi = ['il ragazzo piange' , 'le montagne sono piene di neve', 'il diavolo veste prada', 'il terremoto ha distrutto la citta', 'incendio non ha distrutto nemmeno una casa' ]
+
+    for frase in frasi:
+
+        translator.raise_Exception = True
+
+        print(frase, ' --> ', translator.translate(frase, src='it').text)
+
+        # translations = translator.translate('The quick brown fox', dest='it')
+        # for translation in translations:
+        #     print(translation.origin, ' -> ', translation.text)
+
+        sentence = flair.data.Sentence(frase)
+        sentiment_model.predict(sentence)
+        probability = sentence.labels[0].score  # numerical value 0-1
+        sentiment = sentence.labels[0].value  # 'POSITIVE' or 'NEGATIVE'
+        print('\t prob --> ', probability)
+        print('\t sent --> ', sentiment)
+
+
+
+
+
+
+
+
+    print('...end.')
